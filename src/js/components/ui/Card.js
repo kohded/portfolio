f4857316a7d7e135cd079b3e49e6b451a8ae036f
Arnold Koh <kohded@users.noreply.github.com>
2018-02-10 18:37:12 -0800

    Add props to card component

--- src/js/components/ui/Card.js
+++ src/js/components/ui/Card.js
@@ -7,25 +7,43 @@ const card = (props) => {
   const Element = props.element;
 
   return (
-    <Element className={props.className} styleName="card">
+    <Element
+      aria-label={props.ariaLabel}
+      className={props.className}
+      onClick={props.onClick}
+      onKeyUp={props.onKeyUp}
+      role={props.role}
+      styleName="card"
+      tabIndex={props.tabIndex}
+    >
       {props.children}
     </Element>
   );
 };
 
 card.defaultProps = {
+  ariaLabel: undefined,
   children: undefined,
   className: undefined,
   element: 'div',
+  onClick: undefined,
+  onKeyUp: undefined,
+  role: undefined,
+  tabIndex: -1,
 };
 
 card.propTypes = {
+  ariaLabel: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.element),
     PropTypes.element,
   ]),
   className: PropTypes.string,
   element: PropTypes.string,
+  onClick: PropTypes.func,
+  onKeyUp: PropTypes.func,
+  role: PropTypes.string,
+  tabIndex: PropTypes.number,
 };
 
 export default CSSModules(card, styles);
